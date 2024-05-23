0488ed3d9c03a3ebe1fc07d1d2dee6e90c5630a2
Pavel <p.al.trofimov@gmail.com>
2024-05-23 20:35:54 +0300

    added length of last word solution

---
+++ dataStructure/lengthOfLastWord/src/test/java/ru/leetcode/LengthOfLastWordTest.java
@@ -0,0 +1,38 @@
+package ru.leetcode;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LengthOfLastWordTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LengthOfLastWordTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LengthOfLastWordTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
