91a620bc77c93c9de0cba7b615b7511938897a8a
Pavel <p.al.trofimov@gmail.com>
2024-05-25 12:02:21 +0300

    added binary tree order traversal solution

---
+++ dataStructure/binaryTreeInorderTraversal/src/main/java/org/example/BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(8);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(5);
+
+        //1, 6, 2, 8, 3, 2, 5
+
+        Solution solution = new Solution();
+        List<Integer> result = solution.inorderTraversal(root);
+
+        System.out.println(result);  // [1, 3, 2]
+    }
+}
