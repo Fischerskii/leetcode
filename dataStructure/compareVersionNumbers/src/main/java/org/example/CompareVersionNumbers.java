22668e8bd12acdff7dad39294b7d2bac4a250721
Pavel <p.al.trofimov@gmail.com>
2024-05-31 11:56:06 +0300

    added VersionCompararatorRealisation

---
+++ dataStructure/compareVersionNumbers/src/main/java/org/example/CompareVersionNumbers.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class CompareVersionNumbers {
+    public static void main(String[] args) {
+        System.out.println(new VersionComparator().compareVersion("4.00.01", "4.00.00.01"));
+    }
+}
