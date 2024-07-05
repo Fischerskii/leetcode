0ce7f498ce4d2b9096ef94582fc8829c543715cc
Pavel <p.al.trofimov@gmail.com>
2024-07-05 13:08:00 +0300

    added Word Pattern solution

--- dataStructure/wordPattern/src/main/java/org/example/Solution.java
+++ dataStructure/wordPattern/src/main/java/org/example/Solution.java
@@ -1,2 +1,28 @@
-package org.example;public class Solution {
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        char[] patternChars = pattern.toCharArray();
+        String[] words = s.split(" ");
+
+        if (patternChars.length != words.length) {
+            return false;
+        }
+
+        Map<Character, String> patternLike = new HashMap<>();
+        for (int i = 0; i < patternChars.length; i++) {
+            if (!patternLike.containsKey(patternChars[i]) && !patternLike.containsValue(words[i])) {
+                patternLike.put(patternChars[i], words[i]);
+            } else if ((patternLike.containsKey(patternChars[i]) && !patternLike.get(patternChars[i]).equals(words[i]))
+                    || (!patternLike.containsKey(patternChars[i]) && patternLike.containsValue(words[i]))
+            ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
