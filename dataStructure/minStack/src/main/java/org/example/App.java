e6d840d618a282ee65670d37f5f99bd8196147c6
Pavel <p.al.trofimov@gmail.com>
2024-09-16 17:57:03 +0300

    added final solution

--- dataStructure/minStack/src/main/java/org/example/App.java
+++ dataStructure/minStack/src/main/java/org/example/App.java
@@ -2,12 +2,16 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+//        MinStack obj = new MinStack();
+//        obj.push(-3);
+//        obj.push(-2);
+//        obj.push(1);
+//        obj.pop();
+//        int param_3 = obj.top();
+//        int param_4 = obj.getMin();
     }
 }
