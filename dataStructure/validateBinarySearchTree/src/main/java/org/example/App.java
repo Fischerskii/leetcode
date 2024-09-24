8806b643a0f164a171966bc19d5b37aba4728f78
Pavel <p.al.trofimov@gmail.com>
2024-09-24 12:22:54 +0300

    added ValidateBinarySearchTree solution

---
+++ dataStructure/validateBinarySearchTree/src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(5, new TreeNode(1), new TreeNode(4, new TreeNode(3), new TreeNode(6)));
+        System.out.println(new ValidateBinarySearchTree().isValidBST(treeNode));
+    }
+}
